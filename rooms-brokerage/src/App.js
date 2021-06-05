cc3acc88cb871b1f657160c359b7ff01953d6c2f
Satvinder Pal Singh <satvinderpalsingh52663@gmail.com>
2021-06-05 13:30:50 +0530

    project-structure

--- rooms-brokerage/src/App.js
+++ rooms-brokerage/src/App.js
@@ -1,37 +1,7 @@
 import React from 'react';
-import { useState } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import logo from './logo.svg';
-import './App.css';
-
-import modules from './modules';
-
-function App() {
-  const [currentTab, setCurrentTab] = useState('dashboard');
-  return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <ul className="App-nav">
-            {modules.map(module => ( // with a name, and routes
-                <li key={module.name} className={currentTab === module.name ? 'active' : ''}>
-                  <Link to={module.routeProps.path} onClick={() => setCurrentTab(module.name)}>{module.name}</Link>
-                </li>
-            ))}
-          </ul>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-         >
-          Learn React
-        </a>
-      </header>
-    </div>
-    </Router>
-  );
+const App=()=>{
+    return(
+        <h1>APP</h1>
+    );
 }
-
-export default App;
+export default App
--- rooms-brokerage/src/modules/index.js
+++
@@ -1,9 +0,0 @@
-import Dashboard from './home/dashboard';
-import Login from './home/authentication/login';
-import Register from './home/authentication/register';
-
-export default[
-    Dashboard,
-    Login,
-    Register
-];
