cb04621023417c3041d9307e2aaf63c56cc55a6b
Amit Patle <amitpatle@Amits-MacBook-Air.local>
2021-06-12 16:26:23 +0530

    sidebar content

---
+++ rooms-brokerage/src/Components/Utility/Header/sidebarcontent/sidebar.js
@@ -0,0 +1,65 @@
+import React, { Component, useState } from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as BiIcons from "react-icons/bi"
+import * as FcIcons from "react-icons/fc"
+
+export const SidebarData = [
+
+  {
+    title: 'Become a Seller',
+    path: '/',
+    icon: <AiIcons.AiOutlineHome />,
+    cName: 'nav-text-mobile'
+  }, 
+  {
+    title: 'Home',
+    path: '/',
+    icon: <AiIcons.AiOutlineHome />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'About us',
+    path: '/aboutus',
+    icon: <AiIcons.AiOutlineInfoCircle />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Map',
+    path: '/map',
+    icon: <BiIcons.BiMapPin />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Contact us',
+    path: '/contactus',
+    icon: <AiIcons.AiOutlineContacts />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Help And Faq',
+    path: '/helpandfaq',
+    icon: <BiIcons.BiHelpCircle />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'User Homepage',
+    path: '/userpage',
+    icon: <BiIcons.BiHelpCircle />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Add Details',
+    path: '/addetails',
+    icon: <BiIcons.BiHelpCircle />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Wishlist',
+    path: '/wishlist',
+    icon: <BiIcons.BiHelpCircle />,
+    cName: 'nav-text'
+  }
+
+
+]
