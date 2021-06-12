938c4a0ddd7f98ac2dff08462781934b5b7a13aa
Amit Patle <amitpatle@Amits-MacBook-Air.local>
2021-06-12 15:24:32 +0530

    sidebar and homepage edited

--- rooms-brokerage/src/Components/Home/Homepage/Home.js
+++ rooms-brokerage/src/Components/Home/Homepage/Home.js
@@ -2,49 +2,65 @@ import react from 'react';
 import './Home.css';
 import Banner from './Banner.js';
 import Card from './Card.js';
-
-function Home(){
+import Grid from '@material-ui/core/Grid';
+function Home() {
     return (
         <div className='home'>
             <Banner />
-            <div className='home__section'>
-            <Card
-                src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
-                title="Real Time Support"
-                description="We provide real-time support to our users."
-            />
-            <Card
-                src="https://images.unsplash.com/photo-1503174971373-b1f69850bded?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=787&q=80"
-                title="Wide variety of living spaces"
-                description="Spaces that are more than just a place to sleep."
-            />
-            <Card
-                src="https://a0.muscache.com/im/pictures/fdb46962-10c1-45fc-a228-d0b055411448.jpg?im_w=720"
-                title="Entire homes"
-                description="Comfortable private places, with room for friends or family."
-            />
-            </div>
-            <div className='home__section'>
-            <Card
-                src="https://media.nomadicmatt.com/2019/airbnb_breakup3.jpg"
-                title="Sharing Flats in Mumbai"
-                description="Divides rent alongwith space."
-                price="₹10000/month"
-            />
-            <Card
-                src="https://thespaces.com/wp-content/uploads/2017/08/Courtesy-of-Airbnb.jpg"
-                title="Luxurious Penthouse"
-                description="Enjoy the amazing sights of Mumbai with stunning high-end apartments"
-                price="Starting at ₹2.5 crores"
-            />
-            <Card
-                src="https://media.nomadicmatt.com/2018/apartment.jpg"
-                title="Apartments for Students"
-                description="Get places to stay near your educational institute"
-                price="Starting at ₹6000/month."
-            />
-            </div>
-            
+            <Grid container spacing={24}>
+                <Grid container item xs={12} sm={4}>
+                    <Card
+                        src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+                        title="Real Time Support"
+                        description="We provide real-time support to our users."
+                    />
+                </Grid>
+                <Grid container item item xs={12} sm={4}>
+                    <Card
+                        src="https://images.unsplash.com/photo-1503174971373-b1f69850bded?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=787&q=80"
+                        title="Wide variety of living spaces"
+                        description="Spaces that are more than just a place to sleep."
+                    />
+                </Grid>
+
+                <Grid container item item xs={12} sm={4}>
+                    <Card
+                        src="https://a0.muscache.com/im/pictures/fdb46962-10c1-45fc-a228-d0b055411448.jpg?im_w=720"
+                        title="Entire homes"
+                        description="Comfortable private places, with room for friends or family."
+                    />
+                </Grid>
+
+            </Grid>
+
+            <Grid container spacing={24}>
+                <Grid item item xs={12} sm={4}>
+                    <Card
+                        src="https://media.nomadicmatt.com/2019/airbnb_breakup3.jpg"
+                        title="Sharing Flats in Mumbai"
+                        description="Divides rent alongwith space."
+                        price="₹10000/month"
+                    />
+                </Grid>
+                <Grid item item xs={12} sm={4}>
+                    <Card
+                        src="https://thespaces.com/wp-content/uploads/2017/08/Courtesy-of-Airbnb.jpg"
+                        title="Luxurious Penthouse"
+                        description="Enjoy the amazing sights of Mumbai with stunning high-end apartments"
+                        price="Starting at ₹2.5 crores"
+                    />
+                </Grid>
+                <Grid item item xs={12} sm={4}>
+                    <Card
+                        src="https://media.nomadicmatt.com/2018/apartment.jpg"
+                        title="Apartments for Students"
+                        description="Get places to stay near your educational institute"
+                        price="Starting at ₹6000/month."
+                    />
+                </Grid>
+
+            </Grid>
+
         </div>
     );
 }
--- rooms-brokerage/src/Components/Utility/Header/header.js
+++ rooms-brokerage/src/Components/Utility/Header/header.js
@@ -1,30 +1,69 @@
-import React from 'react';
+import React, { Component, useState } from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import { SidebarData } from './sidebarcontent/sidebar';
 import './header.css';
 import SearchRoundedIcon from '@material-ui/icons/SearchRounded';
 import LanguageIcon from '@material-ui/icons/Language';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import {Button} from '@material-ui/core';
-import {Link} from 'react-router-dom';
+import { Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import Hidden from '@material-ui/core/Hidden';
 
-function Header(){
+
+function Header() {
+    const [sidebar, setSidebar] = useState(false);
+
+    const showSidebar = () => setSidebar(!sidebar);
     return (
         <div className='header'>
-            <Link to ='/'>
-            <img
-                className='header__icon'
-                src="https://images.pexels.com/photos/430205/pexels-photo-430205.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                alt="logo"
-            />
+            <Link to={'#'} >
+                <span class="ml-auto "><AiIcons.AiOutlineBars onClick={showSidebar} /></span>
             </Link>
+            <div className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+                <ul className='nav-menu-items' onClick={showSidebar}>
+                    <li className='navbar-toggle'>
+                        <Link to='#' className='menu-bars'>
+                            <AiIcons.AiOutlineClose />
+                        </Link>
+                    </li>
+                    {SidebarData.map((item, index) => {
+                        return (
+                            <li key={index} className={item.cName}>
+                                <Link to={item.path}>
+                                    {item.icon}
+                                    <span>{item.title}</span>
+                                </Link>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+            <div>
+            <Hidden xsDown>
+                <Link to='/'>
+                    <img
+                        className='header__icon'
+                        src="https://images.pexels.com/photos/430205/pexels-photo-430205.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                        alt="logo"
+                    />
+                </Link>
+            </Hidden>
+            </div>
             <div className='header__center'>
                 <input type='text' />
                 <SearchRoundedIcon />
             </div>
+            <Hidden smDown>
             <div className='header__right'>
                 <div className='header__b'>
                     <Button varient='outlined'>Become a Seller</Button>
                 </div>
+
+            </div>
+            </Hidden>
+            <div>
                 <LanguageIcon />
                 <ExpandMoreIcon />
                 <AccountCircleIcon />
