1739f4f54bbf0849562e59b834a08e3a0af3a634
Amit Patle <amitpatle@Amits-MacBook-Air.local>
2021-07-21 23:04:01 +0530

    database changes

--- Backend/database.js
+++ Backend/database.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mysql = require("mysql");
-const db = mysql.createConnection({
+const db = {
     // user: "root",
     user: "uzey35wx08fxxsmp",
     // host: "localhost",
@@ -10,10 +10,36 @@ const db = mysql.createConnection({
     // database: "LoginSystem",
     database: "byhwavuppyienosgunop",
     insecureAuth: true
-});
-db.query('SELECT NOW()', function(error, result){
-    if(error) throw error
-    console.log(result)
-})
+};
+var connection;
 
-module.exports = db;
+function handleDisconnect() {
+  connection = mysql.createConnection(db); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+
+  connection.query('SELECT NOW()', function(error, result){
+        if(error) throw error
+        console.log(result)
+   });
+}
+
+handleDisconnect();
+
+
+module.exports = connection;
